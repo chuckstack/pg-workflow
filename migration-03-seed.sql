d8d86d853b2bcb6e8f69670bcb7932e7c5743df2
Chuck Boecking <chuck@chuboe.com>
2024-05-31 23:32:56 -0500

    refactored sample data to remove hard-coded uuid dependencies and make the sample data easier to understand and manipulate

--- migration-03-seed.sql
+++ migration-03-seed.sql
@@ -1,14 +1,5 @@
 -- the purpose of this file is to provide records that help kickstart process and request creation.
 
--- things that you might want to create when generating a new process
-  -- state
-  -- activity
-  -- action
-  -- target
-  -- resolution
-  -- transition
-  -- group?
-
 set search_path = private;
 
 ------------------------------------
@@ -19,13 +10,115 @@ CREATE OR REPLACE FUNCTION chuboe_wf_template_create_approval() RETURNS uuid AS 
 DECLARE
     v_process_uu uuid;
 BEGIN
-    set search_path = private;
+    set search_path = private,pg_temp;
     
     --------------------------------
-    -- create the temporary tables
+    -- create the temporary tables and functions
     --------------------------------
-    -- todo: these temp table names are hard-coded; therefore, it might not be concurrent safe. Need to research.
+    -- note: The temp tables and functions are created in the pg_temp space; therefore they are local to the session.
+    -- reference: https://stackoverflow.com/questions/39983154/how-do-i-create-a-nested-function-in-pl-pgsql
     
+    -- create a temporary table to hold search_key, name, description and uuid for to be created values
+    -- the purpose of this table is to make it easy for users to specify what entities get pulled from their respective types.
+    CREATE TEMPORARY TABLE pg_temp.kv(
+        table_name varchar(255) not null,
+        search_key varchar(255) not null,
+        name varchar(255),
+        description text,
+        uu uuid not null default gen_random_uuid(),
+        unique (table_name,search_key)
+    );
+
+    -- Create a temporary convenience function to get the kv.uuid
+    CREATE FUNCTION pg_temp.kvuu(search_key text, table_name text) RETURNS uuid AS $in$
+        BEGIN
+            RETURN (select s.uu from kv s where s.search_key = kvuu.search_key and s.table_name = kvuu.table_name);
+        END;
+        $in$ LANGUAGE plpgsql;
+
+    -- Create a temporary convenience function to get the kv.name
+    CREATE FUNCTION pg_temp.kvname(search_key text, table_name text) RETURNS varchar(255) AS $in$
+        BEGIN
+            RETURN (select s.name from kv s where s.search_key = kvname.search_key and s.table_name = kvname.table_name);
+        END;
+        $in$ LANGUAGE plpgsql;
+
+    -- Create a temporary convenience function to get the kv.description
+    CREATE FUNCTION pg_temp.kvdesc(search_key text, table_name text) RETURNS text AS $in$
+        BEGIN
+            RETURN (select s.description from kv s where s.search_key = kvdesc.search_key and s.table_name = kvdesc.table_name);
+        END;
+        $in$ LANGUAGE plpgsql;
+
+	-- create a temporary table to make it easy for users to specify what transitions should exist.
+	CREATE TEMPORARY TABLE pg_temp.kv_transition(
+        search_key varchar(255) not null,
+        chuboe_state_from_uu uuid not null,
+        chuboe_state_to_uu uuid not null,
+        chuboe_resolution_uu uuid,
+		uu uuid not null default gen_random_uuid()
+    );
+
+	-- Create a temporary convenience function to get the kvt.uuid (transition)
+    CREATE FUNCTION pg_temp.kvtuu(search_key text) RETURNS uuid AS $in$
+        BEGIN
+            RETURN (select s.uu from kv_transition s where s.search_key = kvtuu.search_key);
+        END;
+        $in$ LANGUAGE plpgsql;
+
+    CREATE TEMPORARY TABLE pg_temp.kv_action_transition(
+        chuboe_action_uu uuid not null,
+        chuboe_transition_uu uuid not null,
+        chuboe_resolution_uu uuid,
+		uu uuid not null default gen_random_uuid()
+    );
+
+    -- note: by convention, I am giving each entity the same search_key as its respective type to make type uuid lookups easy.
+    -- example: the chuboe_state search key "started" is the same as the chuboe_state_type.
+    -- The kv table is where you (the user who might be modifying this function) define your custom states, actions, resolution and targets you want in your newly created process.
+	-- insert entities into the temporary table - one value of this step is that it creates uuids for future use
+    INSERT INTO kv (table_name,search_key,name,description) values
+        ('chuboe_process',    'sample_approval', 'Sample approval process', 'Template/example of an approval process.'),
+        ('chuboe_state',      'started',         'New approval request', 'New xxx request'),
+        ('chuboe_state',      'submitted',       'Approval request submitted', 'xxx request submitted to manager'),
+        ('chuboe_state',      'replied',         'Approval request replied', 'Manager replied to xxx request'),
+        ('chuboe_state',      'finalized',       'Approval request final closed', 'xxx request final closed'),
+        ('chuboe_action',     'submit',          'Submit Request', 'Employee submits the xxx request'),
+        ('chuboe_action',     'approve',         'Approve Request', 'Manager approves the xxx request'),
+        ('chuboe_action',     'more-info',       'Request More Info', 'Manager needs more informationo to approve  xxx request'),
+        ('chuboe_action',     'deny',            'Deny Request', 'Manager denies the xxx request'),
+        ('chuboe_action',     'close',           'Close Request', 'Employee closes the xxx request as acknowledgement of the response'),
+        ('chuboe_resolution', 'none', 			  null, null),
+        ('chuboe_resolution', 'approved', 		  null, null),
+        ('chuboe_resolution', 'pending', 		  null, null),
+        ('chuboe_resolution', 'denied', 		  null, null),
+        ('chuboe_group',      'managers', 		  null, null),
+        ('chuboe_target',     'requester', 	     'Requester', 'The user who initiated the request.'),
+        ('chuboe_target',     'stakeholder',  	 'Stakeholders', 'The users who are stakeholders of the request.'),
+        ('chuboe_target',     'group_member',    'Group Members', 'The users who are members of the group associated with the request.'),
+        ('chuboe_target',     'process_admin',   'Process Admins', 'The users who are administrators of the process associated with the request.')
+    ;
+    
+	-- insert transitions into the temporary table - one value of this step is that it creates uuids for future use
+    INSERT INTO kv_transition (search_key, chuboe_state_from_uu, chuboe_state_to_uu, chuboe_resolution_uu) values
+        ('started-submitted', pg_temp.kvuu('started','chuboe_state'),  pg_temp.kvuu('submitted','chuboe_state'),null),
+        ('submitted-replied', pg_temp.kvuu('submitted','chuboe_state'),pg_temp.kvuu('replied','chuboe_state'),  null),
+        ('submitted-started', pg_temp.kvuu('submitted','chuboe_state'),pg_temp.kvuu('started','chuboe_state'),  null),
+        ('replied-finalized', pg_temp.kvuu('replied','chuboe_state'),  pg_temp.kvuu('finalized','chuboe_state'),null)
+	;
+    
+	-- insert transitions into the temporary table - one value of this step is that it creates uuids for future use
+    INSERT INTO kv_action_transition (chuboe_action_uu, chuboe_transition_uu, chuboe_resolution_uu) values
+        (pg_temp.kvuu('submit','chuboe_action'),  pg_temp.kvtuu('started-submitted'),pg_temp.kvuu('pending','chuboe_resolution')),
+        (pg_temp.kvuu('approve','chuboe_action'),pg_temp.kvtuu('submitted-replied'),  pg_temp.kvuu('approved','chuboe_resolution')),
+        (pg_temp.kvuu('more-info','chuboe_action'),pg_temp.kvtuu('submitted-started'),  pg_temp.kvuu('pending','chuboe_resolution')),
+        (pg_temp.kvuu('deny','chuboe_action'),pg_temp.kvtuu('submitted-replied'),  pg_temp.kvuu('denied','chuboe_resolution')),
+        (pg_temp.kvuu('close','chuboe_action'),  pg_temp.kvtuu('replied-finalized'),null)
+	;
+    
+
+
+	-- todo delete me and refactor
     -- state, action and resolution
     CREATE TEMPORARY TABLE chuboe_wf_template_temp_01(
         search_key varchar(255),
@@ -33,6 +126,8 @@ BEGIN
         description varchar(255) 
     );
     
+	-- todo rename me and refactor - add to pg_temp schema
+	-- todo remove many of the uuid or key columns since should not be needed
     -- transition
     CREATE TEMPORARY TABLE chuboe_wf_template_temp_02_transition(
         temp_uu UUID PRIMARY KEY DEFAULT gen_random_uuid(), --allows us to pre-determine the id for future purposes
@@ -45,6 +140,8 @@ BEGIN
         chuboe_process_uu uuid
     );
  
+	-- todo rename me and refactor - add to pg_temp schema
+	-- todo remove many of the uuid or key columns since should not be needed
     -- acton-transition
     CREATE TEMPORARY TABLE chuboe_wf_template_temp_03_action_transition(
         temp_uu UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -59,6 +156,7 @@ BEGIN
     --------------------------------
     -- create the process
     --------------------------------
+	-- todo use kv table
     INSERT INTO chuboe_process (search_key, name, is_template, description, chuboe_process_type_uu)
     VALUES ('sample_approval', 'Sample approval process', true, 'Template/example of an approval process.', 'd4d85948-ee7e-4880-8269-e5cc85b3f2fe')
     RETURNING chuboe_process_uu INTO v_process_uu;
