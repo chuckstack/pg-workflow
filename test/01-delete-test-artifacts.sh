67f02a9d0701536c76c1d9ad36b63d6afba6bb76
Chuck Boecking <chuck@chuboe.com>
2024-06-24 15:03:46 -0500

    working with test including api schema and postgrest roles - postgrest inself not tested

--- test/01-delete-test-artifacts.sh
+++ test/01-delete-test-artifacts.sh
@@ -21,6 +21,24 @@ echo "psql_test: $psql_test"
 #echo $SCHEMA_EXISTS
 
 #if you get an error that the user or schema does not exist, simply comment out these lines temporarily
+psql $psql_test -c " alter default privileges in schema $TEST_SCHEMA_API revoke select on tables from $TEST_USER_PREST_AUTH; "
+psql $psql_test -c " revoke select on all tables in schema $TEST_SCHEMA_API from $TEST_USER_PREST_AUTH "
+psql $psql_test -c " revoke create, usage on schema $TEST_SCHEMA_API from $TEST_USER_PREST_AUTH "
+psql $psql_test -c " revoke all privileges on database $TEST_DB from $TEST_USER_PREST_AUTH "
+psql $psql_test -c " revoke connect on database $TEST_DB from $TEST_USER_PREST_AUTH "
+psql $psql_test -c " reassign owned by $TEST_USER_PREST_AUTH to $TEST_USER_ADMIN "
+psql $psql_test -c " drop owned by $TEST_USER_PREST_AUTH "
+psql $psql_admin -c " drop user if exists $TEST_USER_PREST_AUTH "
+
+psql $psql_test -c " alter default privileges in schema $TEST_SCHEMA_API revoke select on tables from $TEST_USER_PREST_ANON; "
+psql $psql_test -c " revoke select on all tables in schema $TEST_SCHEMA_API from $TEST_USER_PREST_ANON "
+psql $psql_test -c " revoke create, usage on schema $TEST_SCHEMA_API from $TEST_USER_PREST_ANON "
+psql $psql_test -c " revoke all privileges on database $TEST_DB from $TEST_USER_PREST_ANON "
+psql $psql_test -c " revoke connect on database $TEST_DB from $TEST_USER_PREST_ANON "
+psql $psql_test -c " reassign owned by $TEST_USER_PREST_ANON to $TEST_USER_ADMIN "
+psql $psql_test -c " drop owned by $TEST_USER_PREST_ANON "
+psql $psql_admin -c " drop user if exists $TEST_USER_PREST_ANON "
+
 psql $psql_test -c " alter default privileges in schema $TEST_SCHEMA revoke select on tables from $TEST_USER; "
 psql $psql_test -c " revoke select on all tables in schema $TEST_SCHEMA from $TEST_USER "
 psql $psql_test -c " revoke create, usage on schema $TEST_SCHEMA from $TEST_USER "
@@ -29,4 +47,5 @@ psql $psql_test -c " revoke connect on database $TEST_DB from $TEST_USER "
 psql $psql_test -c " reassign owned by $TEST_USER to $TEST_USER_ADMIN "
 psql $psql_test -c " drop owned by $TEST_USER "
 psql $psql_admin -c " drop user if exists $TEST_USER "
+
 psql $psql_admin -c " drop database if exists $TEST_DB "
--- test/02-create-test-artifacts.sh
+++ test/02-create-test-artifacts.sh
@@ -9,20 +9,30 @@ cd $TEST_SCRIPTPATH
 
 source test.properties
 
-psql_admin="$TEST_PSQL_HOST $TEST_PSQL_ERR_STOP $TEST_PSQL_USER_ADMIN $TEST_PSQL_DB_ADMIN"
-psql_test="$TEST_PSQL_HOST $TEST_PSQL_ERR_STOP $TEST_PSQL_USER_ADMIN $TEST_PSQL_DB"
-echo "psql_admin: $psql_admin"
+psql_su="$TEST_PSQL_HOST $TEST_PSQL_ERR_STOP $TEST_PSQL_USER_ADMIN $TEST_PSQL_DB_ADMIN"
+psql_su_test="$TEST_PSQL_HOST $TEST_PSQL_ERR_STOP $TEST_PSQL_USER_ADMIN $TEST_PSQL_DB"
+psql_test="$TEST_PSQL_HOST $TEST_PSQL_ERR_STOP $TEST_PSQL_USER $TEST_PSQL_DB"
+echo "psql_su: $psql_su"
+echo "psql_su_test: $psql_su_test"
 echo "psql_test: $psql_test"
 
 #echo '-------begin xxx-------'
 #echo '-------end xxx-------'
 
-psql $psql_admin -c " create database $TEST_DB "
+psql $psql_su -c " create database $TEST_DB "
+psql $psql_su -c " create role $TEST_USER with login password '$TEST_USER_PASSWORD'"
+psql $psql_su -c " create role $TEST_USER_PREST_ANON nologin "
+psql $psql_su -c " create role $TEST_USER_PREST_AUTH noinherit login password '$TEST_USER_PREST_AUTH_PASSWORD' "
+psql $psql_su -c " grant $TEST_USER_PREST_ANON to $TEST_USER_PREST_AUTH "
+psql $psql_su -c " grant create, connect on database $TEST_DB to $TEST_USER "
+
 psql $psql_test -c " create schema $TEST_SCHEMA "
-psql $psql_test -c " create role $TEST_USER with login password '$TEST_USER_PASSWORD'"
-psql $psql_test -c " grant create, connect on database $TEST_DB to $TEST_USER "
 psql $psql_test -c " grant create, usage on schema $TEST_SCHEMA to $TEST_USER "
 psql $psql_test -c " grant select, insert, update, delete on all tables in schema $TEST_SCHEMA to $TEST_USER "
 psql $psql_test -c " alter default privileges in schema $TEST_SCHEMA grant select on tables to $TEST_USER "
-psql $psql_test -c " alter role $TEST_USER set search_path to $TEST_SCHEMA "
 
+psql $psql_test -c " create schema $TEST_SCHEMA_API "
+psql $psql_test -c " grant usage on schema $TEST_SCHEMA_API to $TEST_USER_PREST_ANON "
+psql $psql_test -c " grant all on all tables in schema $TEST_SCHEMA_API to $TEST_USER_PREST_ANON "
+
+psql $psql_test -c " alter role $TEST_USER set search_path to $TEST_SCHEMA,$TEST_SCHEMA_API "
